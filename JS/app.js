97b13bca7e07152d638fbce7c284c505984af081
Donovan <donniemassenburg75@gmail.com>
2022-06-09 19:28:46 -0400

    added questions

--- JS/app.js
+++ JS/app.js
@@ -1,28 +1,38 @@
-const fs = require('fs')
-const generatePage = require('./page-template');
+const inquirer = require('inquirer')
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is you Name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
-const [name, github] = profileDataArgs
 
 
 
-  fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
-  
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
-// const printProfileData = profileDataArr => {
-//   // This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
 
-//   console.log('================');
 
-//   // Is the same as this...
-//   profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
 
-// printProfileData(profileDataArgs);
+
+
+
+
+
+// const fs = require('fs')
+// const generatePage = require('./page-template');
+
+// const pageHTML = generatePage(name, github);
+
+// const [name, github] = profileDataArgs
+
+
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
